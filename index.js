ec8b4536bda4da84906e1ab76bad73abd60b77ca
+ <+>
2023-09-13 11:49:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function makeInt(string) {
+    // Parse the string into an integer with base 10
+    return parseInt(string, 10);
+  }
+  function preserveDecimal(string) {
+    // Parse the string into a floating-point number
+    return parseFloat(string);
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function divide(a, b) {
+    return a/b;
+  }
+  function multiply (a, b) {
+    return a  * b;
+  }
+  function increment (n) {
+    return n + 1;
+  }
+  function decrement (n) {
+    return n - 1;
+  }
